3fcd9fdc39507cbb3493a9a774870a4c34270d08
rbsramya <rbs.ramya@gmail.com>
2025-07-10 21:20:35 +0800

    Fix Terraform formatting

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
-    key    = "ramya-workflow.tfstate"  #Change this
+    key    = "ramya-workflow.tfstate" #Change this
     region = "ap-southeast-1"
   }
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix = "ramyasctp-module2.3"  # Set your bucket name here
+  bucket_prefix = "ramyasctp-module2.3" # Set your bucket name here
 }
 
